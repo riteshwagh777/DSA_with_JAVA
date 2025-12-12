72cc14197a61fd5313e7dcd09b67a72ecb7353ef
riteshwagh777 <waghritesh777@gmail.com>
2025-12-12 20:16:12 +0530

    Create Sum_of_Two_Numbers.java

---
+++ DAY_01/SET_1/Sum_of_Two_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Sum_of_Two_Numbers{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter two numbers");
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int sum= a+b;
+        System.out.println("Sum of this numbers is " +sum);
+    }
+
+}
