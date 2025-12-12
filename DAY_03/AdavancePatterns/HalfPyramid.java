f7f7f45efa8ccfdf6cdbeb82b395c90cff163e8c
Ritesh Wagh <waghritesh777@gmail.com>
2025-12-12 23:53:41 +0530

    Create HalfPyramid.java

---
+++ DAY_03/AdavancePatterns/HalfPyramid.java
@@ -0,0 +1,24 @@
+
+public class HalfPyramid {
+
+    public static void half_pyaramid(int n) {
+    for(int i= 1; i<=n; i++){
+        //spaces 
+        for(int j=1; j<=n-i; j++){
+            System.out.print(" ");
+        }
+        // stars 
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+            System.out.println();
+            }
+
+
+    }
+    
+
+    public static void main(String args[]){
+     half_pyaramid(10);
+    }
+}
