e6e9879217bcf8de7ef6d5b8f89ad4f2980ab8da
Ritesh Wagh <waghritesh777@gmail.com>
2025-12-21 23:00:00 +0530

    Create IncreasingOderNumbers.java

---
+++ DAY_11/Recursion/IncreasingOderNumbers.java
@@ -0,0 +1,20 @@
+public class IncreasingOderNumbers {
+
+    public static void printInc(int n){
+        if(n==1){
+            System.out.print(n + " ");
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n + " ");
+         {
+
+        }
+
+    }
+    public static void main(String args[]){
+        int n = 5;
+        printInc(n);
+
+    }
+}
