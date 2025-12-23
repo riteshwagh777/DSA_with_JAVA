bcc2c192a5b99f755278e11826d185a2e49ff5ba
Ritesh Wagh <waghritesh777@gmail.com>
2025-12-23 23:24:55 +0530

    Create FriendsPairing.java

---
+++ DAY_12/Recursion_2/FriendsPairing.java
@@ -0,0 +1,22 @@
+public class FriendsPairing {
+   public static int friendsPairing(int n) {
+    // base case
+    if(n == 1 || n == 2) {
+        return n;
+    }
+
+    // kaam
+    // single
+    int fnm1 = friendsPairing(n - 1);
+
+    // pair
+    int fnm2 = friendsPairing(n - 2);
+    int pairWays = (n - 1) * fnm2;
+
+    int totWays = fnm1 + pairWays;
+    return totWays;
+}
+public static void main(String args[]){
+    System.out.println(friendsPairing(4));
+} 
+}
